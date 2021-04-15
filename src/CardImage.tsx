f9cc60594b6fcb93b7a476112bf9470c040115b3
VictorBarros <victorbarros1130@gmail.com>
2021-04-15 16:41:47 -0300

    improve lock handler

--- src/CardImage.tsx
+++ src/CardImage.tsx
@@ -27,10 +27,12 @@ export const CardImage: React.FC<CardImageProps> = ({card, isLocked, ...props}: 
   )
 }
 
-export const GetCardImageSource = ({color, media}: Card, isLocked?: Boolean) => {
-  if (isLocked) return require(`../assets/card/BCBCBC.png`)
+export const GetCardImageSource = (card: Card, isLocked?: Boolean) => {
+  if ((isLocked === undefined && card.isLocked === true) || isLocked === true) {
+    return require(`../assets/card/BCBCBC.png`)
+  }
 
-  switch(color.valueOf()){
+  switch(card.color.valueOf()){
     case "#44FF4E": return require(`../assets/card/44FF4E.png`)
     case "#FFB131": return require(`../assets/card/FFB131.png`)
     case "#5ED4DC": return require(`../assets/card/5ED4DC.png`)
@@ -43,7 +45,7 @@ export const GetCardImageSource = ({color, media}: Card, isLocked?: Boolean) => 
     case "#03C09A": return require(`../assets/card/03C09A.png`)
     case "#FBEAC6": return require(`../assets/card/FBEAC6.png`)
     default: {
-      if (media === CardMedia.VIRTUAL) {
+      if (card.media === CardMedia.VIRTUAL) {
         return require(`../assets/card/44FF4E.png`)
       }
       return require(`../assets/card/2B2B2B.png`)
