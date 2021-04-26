6f3d9e7c6b21a972a17a357345431c4f2e7964fe
VictorBarros <victorbarros1130@gmail.com>
2021-04-26 18:31:28 -0300

    move styles

--- src/KExitButton.tsx
+++ src/KExitButton.tsx
@@ -11,21 +11,15 @@ export interface KExitButtonProps {
 const KExitButton: React.FC<KExitButtonProps> = ({ onPress, buttonRadius }: KExitButtonProps) => {
   const radius = buttonRadius || 25
 
-  const buttonStyles = StyleSheet.create({
-    button: {
-      margin: (radius / 2),
-      backgroundColor: "#FFFFFF",
-      height: (2 * radius),
-      width: (2 * radius),
-      borderRadius: radius,
-      elevation: (radius / 3),
-      justifyContent: "center",
-    }
-  })
-
   return (
     <Pressable onPress={onPress}>
-      <View style={buttonStyles.button}>
+      <View style={[styles.button, {
+        margin: (radius / 2),
+        height: (2 * radius),
+        width: (2 * radius),
+        borderRadius: radius,
+        elevation: (radius / 3)
+      }]}>
         <Icon name="times" size={radius} color="black" style={{alignSelf: "center"}} />
       </View>
     </Pressable>
@@ -33,3 +27,10 @@ const KExitButton: React.FC<KExitButtonProps> = ({ onPress, buttonRadius }: KExi
 }
 
 export default KExitButton
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#FFFFFF",
+    justifyContent: "center",
+  }
+})
