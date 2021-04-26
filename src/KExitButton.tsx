5a1a284a0762ec0d9f11ce401a1dae8a53aa4f10
VictorBarros <victorbarros1130@gmail.com>
2021-04-26 18:44:23 -0300

    refact

--- src/KExitButton.tsx
+++ src/KExitButton.tsx
@@ -1,25 +1,24 @@
-import React from "react"
-import { StyleSheet, View, Pressable } from "react-native"
+import React, { useState } from "react"
+import { Pressable, View, StyleSheet } from "react-native"
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-export interface KExitButtonProps {
-  onPress: () => void
+export interface KExitButtonProps  {
+  onPress?: () => void
   buttonRadius?: number
 }
 
-const KExitButton: React.FC<KExitButtonProps> = ({ onPress, buttonRadius }: KExitButtonProps) => {
-  const radius = buttonRadius || 25
+export const KExitButton: React.FC<KExitButtonProps> = ({onPress, buttonRadius}: KExitButtonProps) => {
+  const [radius, setradius] = useState<number>(buttonRadius || 25)
 
   return (
     <Pressable onPress={onPress}>
-      <View style={[styles.button, {
+      <View style={{
         margin: (radius / 2),
         height: (2 * radius),
         width: (2 * radius),
         borderRadius: radius,
         elevation: (radius / 3)
-      }]}>
+      }}>
         <Icon name="times" size={radius} color="black" style={{alignSelf: "center"}} />
       </View>
     </Pressable>
@@ -27,10 +26,3 @@ const KExitButton: React.FC<KExitButtonProps> = ({ onPress, buttonRadius }: KExi
 }
 
 export default KExitButton
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#FFFFFF",
-    justifyContent: "center",
-  }
-})
