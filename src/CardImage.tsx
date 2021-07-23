16bd1e1f39153548e9913b2149e299dff6ee86ca
VictorBarros <victorbarros1130@gmail.com>
2021-07-23 17:04:11 -0300

    add sensitive data to card image

--- src/CardImage.tsx
+++ src/CardImage.tsx
@@ -9,9 +9,14 @@ import Logo from "./Logo"
 export interface CardImageProps extends ViewProps {
   card: Card
   isLocked?: boolean
+  sensitiveData?: {
+    cardNumber?: string
+    cvv?: string
+  }
+  showSensitiveData?: boolean
 }
 
-export const CardImage: React.FC<CardImageProps> = ({card, isLocked, ...props}: CardImageProps) => {
+export const CardImage: React.FC<CardImageProps> = ({card, isLocked, sensitiveData: { cardNumber = "", cvv = "" } = {}, showSensitiveData = false, ...props}: CardImageProps) => {
   if (isLocked === undefined) isLocked = card.isLocked
   const wordColor: string = (card.color === CardColor.BLACK && !(isLocked)) ? "white" : "black"
 
@@ -19,6 +24,28 @@ export const CardImage: React.FC<CardImageProps> = ({card, isLocked, ...props}: 
     <View style={{alignItems: 'center'}}>
       <View style={style.cardView} {...props}>
         <Image style={style.cardImage} source={GetCardImageSource(card, isLocked)} />
+            {showSensitiveData ?
+                <View style={[style.sensitiveData, { height: 210, justifyContent: "space-between" }]}>
+                    <KText style={{ lineHeight: 18, color: wordColor }}>
+                        {`${cardNumber.substring(0, 4)}\n${cardNumber.substring(4, 8)}\n${cardNumber.substring(8, 12)}\n${cardNumber.substring(12, 16)}`}
+                    </KText>
+                    <View>
+                        <KText>{`CVV ${cvv}`}</KText>
+                        <KText>{`EXP ${card.expirationDate}`}</KText>
+                    </View>
+                </View>
+                :
+                <View style={style.sensitiveData}>
+                    {[1, 2, 3].map(() => (
+                        <View style={{
+                            height: 7,
+                            backgroundColor: "black",
+                            marginVertical: 3,
+                        }} />
+                    ))}
+                    <KText style={{ lineHeight: 18, color: wordColor }}>{card.lastFour}</KText>
+                </View>
+            }
         <Logo style={style.logo} color={wordColor} />
         <KText style={[style.cardName, {color: wordColor}]}>{card?.name}</KText>
       </View>
@@ -58,6 +85,11 @@ const style = StyleSheet.create({
   cardView: {
   },
   cardImage: {
+  },
+  sensitiveData: {
+    position: "absolute",
+    paddingVertical: 20,
+    paddingLeft: 20,
   },
   cardName: {
     position: "absolute",
