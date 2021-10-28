2d92ec4cf970ed7dc74879d2b54a7cf21bbf02db
VictorBarros <victorbarros1130@gmail.com>
2021-10-28 13:52:44 -0300

    add KKeyboardAvoidingView

---
+++ src/KKeyboardAvoidingView.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import { KeyboardAvoidingView, ViewProps } from "react-native"
+
+export const KKeyboardAvoidingView = (props: ViewProps) => (<KeyboardAvoidingView {...props} />)
+
+
+export default KKeyboardAvoidingView
