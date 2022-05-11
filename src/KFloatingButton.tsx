4ca05edd5f6d4bfc1233d0edd1c509455b7d2b75
VictorBarros <victorbarros1130@gmail.com>
2022-05-11 16:51:18 -0300

    parametrize size

--- src/KFloatingButton.tsx
+++ src/KFloatingButton.tsx
@@ -7,11 +7,14 @@ import Svg, { Path } from "react-native-svg"
 export interface KFloatingButtonProps extends ViewProps {
     onPress: () => void
     vibrationFeedback?: boolean
+    size?: "large" | "small"
 }
 
-export const KFloatingButton: React.FC<KFloatingButtonProps> = ({onPress, vibrationFeedback = false, ...props} : KFloatingButtonProps) => {
+export const KFloatingButton: React.FC<KFloatingButtonProps> = ({onPress, vibrationFeedback = false, size = "small", ...props} : KFloatingButtonProps) => {
     const dimensions = useWindowDimensions()
-    const left = (dimensions.width / 2) - 25
+    const width = size == "small" ? 50 : 65
+    const height = width
+    const left = (dimensions.width / 2) - width/2
 
     var onButtonPress = (() => {
         if (vibrationFeedback === true) Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
@@ -20,8 +23,7 @@ export const KFloatingButton: React.FC<KFloatingButtonProps> = ({onPress, vibrat
 
 
     return (
-    
-    <View style={[style.kfloatingButton, {left: left}]} {...props}>
+    <View style={[style.kfloatingButton, {left , width, height, borderRadius: width/2}]} {...props}>
         <Pressable onPress={onButtonPress} hitSlop={20}>
         <Svg width={16} height={16} fill="none" >
             <Path d="M15 15L8 8l7-7M1 15l7-7-7-7" stroke="#191919" strokeWidth={2} />
@@ -35,12 +37,12 @@ export default KFloatingButton
 
 const style = StyleSheet.create({
     kfloatingButton:{
-        height: 50,
-        width: 50,
-        borderRadius: 50,
+        // height: 50,
+        // width: 50,
+        // borderRadius: 50,
         elevation: 10,
         position: "absolute",
-        bottom: 20,        
+        bottom: 30,        
         zIndex: 2,
         backgroundColor: "white",
         justifyContent: "center",
