2abdd34c133b09634b11a4e2ce3142aeecaaf17f
VictorBarros <victorbarros1130@gmail.com>
2022-06-29 19:55:52 -0300

    add panel size prop

--- src/KMiniAppPanel.tsx
+++ src/KMiniAppPanel.tsx
@@ -6,14 +6,20 @@ import { View, StyleSheet } from "react-native"
 
 export interface KTransactionPanelProps {
     recipeName: string,
-    panelStyle: "transaction" | "home"
+    panelStyle?: "transaction" | "home"
+    panelSize?: "small" | "medium" | "large"
 }
 
-export const KMiniAppPanel: React.FC<KTransactionPanelProps> = ({recipeName, children, panelStyle, ...props} : PropsWithChildren<KTransactionPanelProps>) => {
+export const KMiniAppPanel: React.FC<KTransactionPanelProps> = ({recipeName, children, panelStyle, panelSize} : PropsWithChildren<KTransactionPanelProps>) => {
     
+    if (panelStyle === "home") {
+        panelSize = "large"
+    } else if (panelStyle === "transaction"){
+        panelSize = "small"
+    }
 
     return (
-        <View style={[style.kTransactionPanel, (panelStyle == "home" && style.homePanel)]}>
+        <View style={[style.kTransactionPanel, panelSize && panelStyles[panelSize]]}>
             <KText style={style.recipeName} fontWeight="semibold">{recipeName}</KText>
             <View style={style.templateContainer}>
                 {children}
@@ -24,10 +30,16 @@ export const KMiniAppPanel: React.FC<KTransactionPanelProps> = ({recipeName, chi
 
 export default KMiniAppPanel
 
+const panelStyles = StyleSheet.create({
+    small: { height: 105, },
+    medium: { height: 130, },
+    large: { height: 220, }
+})
+
 const style = StyleSheet.create({
     kTransactionPanel: {
         marginTop: 10,        
-        height: 100,        
+        height: panelStyles.small.height,        
         width: "99.9%",
         backgroundColor: "white",
         paddingHorizontal: 16,
@@ -37,10 +49,7 @@ const style = StyleSheet.create({
             height: 2,            
         },
         shadowOpacity: .1,
-    },
-    homePanel: {
         paddingVertical: 20,
-        height: 225
     },
     recipeName: {
         textTransform: "uppercase",
